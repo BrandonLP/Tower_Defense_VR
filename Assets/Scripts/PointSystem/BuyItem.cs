24058e870cce7c7ffd7f95dd713b21866223607e
sabrinako <shkteddy@gmail.com>
2018-02-26 09:50:10 -0500

    sword snap zone added
    point system not fully implemented

---
+++ Assets/Scripts/PointSystem/BuyItem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuyItem : MonoBehaviour {
+	[Tooltip("How much the item costs")]
+	private int _price;
+
+	[Tooltip("Name of Snap Drop Zone in the hierarchy")]
+	private string _snapName;
+
+	// Use this for initialization
+	void Start () {
+		//Event snapHandler = GameObject.Find (_snapName).GetComponent<VRTK.VRTK_SnapDropZone> ().OnObjectUnsnappedFromDropZone;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//if (snapHandler != null && GameObject.Find("EnemyController").GetComponent<PointTracker>().currentPoints >= _price) {
+		//	GameObject.Find ("EnemyController").GetComponent<PointTracker> ().currentPoints -= _price;
+		//}
+	}
+}
