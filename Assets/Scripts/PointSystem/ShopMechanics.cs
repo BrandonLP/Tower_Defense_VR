8d114a7c504e73b3d5c3f1729745834590a7e1ad
sabrinako <shkteddy@gmail.com>
2018-02-28 01:04:21 -0500

    updated points/shop, enemy movement/waves

---
+++ Assets/Scripts/PointSystem/ShopMechanics.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class ShopMechanics : MonoBehaviour {
+	[Tooltip("How much the item costs")]
+	private int _price;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<VRTK_InteractableObject>().InteractableObjectUnsnappedFromDropZone += new ControllerInteractionEventHandler(UnsnappedFromZone);
+	}
+
+	private void UnsnappedFromZone() {
+		if (GameObject.Find ("EnemyController").GetComponent<PointTracker> ().currentPoints >= _price) {
+			GameObject.Find ("EnemyController").GetComponent<PointTracker> ().currentPoints -= _price;
+		} else {
+			Destroy (this);
+		}
+	}
+}
